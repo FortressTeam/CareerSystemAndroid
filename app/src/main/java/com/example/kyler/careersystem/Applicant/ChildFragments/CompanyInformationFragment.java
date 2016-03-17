4aacb9b1a454729be9df153171472b2d6368929c
duckky93 <kimkhoa20793@gmail.com>
2016-03-17 17:43:08 +0700

    fix UI and in progressing of design company information

---
+++ app/src/main/java/com/example/kyler/careersystem/Applicant/ChildFragments/CompanyInformationFragment.java
@@ -0,0 +1,11 @@
+package com.example.kyler.careersystem.Applicant.ChildFragments;
+
+import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kyler.careersystem.R;
+
+public class CompanyInformationFragment extends Fragment {
+
+}
